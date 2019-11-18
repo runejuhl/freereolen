2f4d9a44578efdfe4399279a5f7d2eed4419113a
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2019-11-18 22:32:11 +0100

    Add check for external URLs

--- anonymize.sh
+++ anonymize.sh
@@ -10,3 +10,9 @@ if [[ -n "${OPF_BOOK_ID_ORIGINAL}" ]]; then
                 -exec grep -q "${OPF_BOOK_ID_ORIGINAL}" {} \; -printf '%p\n' | \
              sort | uniq)
 fi
+
+# Check for URLs that lead to external pages
+if urls=$(grep -rPo --include='**.htm' --include='**.html' --include='**.xhtml' '(?<=")http[^"]+(?=")' "${OEBPS}/Text/" | \
+            grep -E --file=valid-urls.list --invert-match); then
+  error 42 "Files contains URLs that you might want to redact:\\n${urls}"
+fi
